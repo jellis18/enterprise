3c2ec8f4c9451b3a95011dd1f1fcdc953a3c8a0a
Justin Ellis <justin.ellis18@gmail.com>
2017-07-04 15:32:03 -0700

    Try again

--- docs/conf.py
+++ docs/conf.py
@@ -305,7 +305,7 @@ def run_apidoc(_):
         # If we are, assemble the path manually
         cmd_path = os.path.abspath(os.path.join(sys.prefix, 'bin', 'jupyter'))
     os.system('ls _static/notebooks')
-    os.system('jupyter nbconvert --template docs/nb-rst.tpl --to rst _static/notebooks/*.ipynb --output-dir {}'.format(output_path))
+    os.system('jupyter nbconvert --template nb-rst.tpl --to rst _static/notebooks/*.ipynb --output-dir {}'.format(output_path))
 
     #subprocess.check_call([cmd_path, 'nbconvert --template nb-rst.tpl --to rst',
 #                           nb, '--output-dir', output_path])
